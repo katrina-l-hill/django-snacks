cefd17b198bf754dde449f91531402709526c3cd
Katrina Hill <khill365@gmail.com>
2022-05-23 16:22:35 -0700

    added file for urls.py under app; added include path in project urls.py file

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# instructor wants to use template view instead of default import
+from django.views.generic import TemplateView
 
-# Create your views here.
+# in order to use TemplateView, need to add a sub-class and extend TemplateView
+# for a given templete, this view will be responsible for making the markup available so that it can be rendered in teh browser
+class HomePageView(TemplateView):
+    # TemplateView needs one thing from the sub-class
+    template_name = "home.html"
